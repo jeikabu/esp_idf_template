5ac18f341dd15318accf88f111adfec1d56ba556
jeikabu <jeikabu@users.noreply.github.com>
2021-02-10 18:03:03 +0200

    Updates for ESP IDF v4.2

--- install-ssh.sh
+++ install-ssh.sh
@@ -18,7 +18,7 @@ else
     echo ""
     echo "Loading firmware: $FIRMWARE"
     echo ""
-    tar cf - *.bin bootloader/*.bin | ssh pi@$1 "tar xf - -C /tmp;sudo voice_esp32_reset;voice_esptool --chip esp32 --port /dev/ttyS0 --baud 115200 --before default_reset --after hard_reset write_flash -u --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 /tmp/bootloader/bootloader.bin 0x10000 /tmp/$FIRMWARE 0x8000 /tmp/partitions_singleapp.bin"
+    tar cf - *.bin bootloader/*.bin | ssh pi@$1 "tar xf - -C /tmp;sudo voice_esp32_reset;voice_esptool --chip esp32 --port /dev/ttyS0 --baud 115200 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x1000 /tmp/bootloader/bootloader.bin 0x10000 /tmp/$FIRMWARE 0x8000 /tmp/partitions_singleapp.bin"
     echo "done"
     echo ""
     echo "[SUCCESS] Please disconnect your MatrixVoice from the RaspberryPi and reconnect it alone for future OTA updates."
